9046169f2eaf3642d6ee9738e28d93bd0c9157ef
myUsername <myemail@gmail.com>
2023-03-20 11:43:58 -0700

    I have shit the bed basically

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,28 @@
-#include "main.h"
-
+# include "main.h"
 /**
- * print_sign - Determines if the input number
- * greater, equal or less than zero.
+ * print_sign-check the sign of a number.
  *
- * @n: The input number is an integer.
- *
- * Return: 1 is greater than zero. 0 is zero.
- * -1 is less than zero.
+ * @n:Number whose sign is to be checked.
+ * Return:1-if n is greater than 0: 0- if n equals 0:-1-if n is less than 0
  */
+
+
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	        _putchar(43);
-	        return (1);
+		_putchar('+');
+		return (1);
 	}
-        else if (n < 0)
+	else if (n < 0)
 	{
-	        _putchar(45);
-	        return (-1);
+		_putchar ('-');
+		return (-1);
 	}
-        else
+	else
 	{
-	       _putchar(48);
-                return (0);
-        }
-        _putchar('\n');	
+		_putchar('0');
+		return (0);
+	}
+}
