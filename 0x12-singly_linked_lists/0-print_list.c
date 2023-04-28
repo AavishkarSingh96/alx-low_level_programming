e6f63a7758b29e0e2477e1166eebf751b88f04c0
AavishkarSingh <heroaavi@gmail.com>
2023-04-28 16:21:54 -0700

    I am tired

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,13 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t donkey = 5;
+	size_t donkey = 0;
 
 	while (h)
 	{
 
 		if (!h->str)
-			printf("[5] (nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
