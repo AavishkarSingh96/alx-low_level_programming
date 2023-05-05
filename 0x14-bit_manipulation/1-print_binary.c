45a0a82cbea89b0b506582e311b056d750863eb4
AavishkarSingh <heroaavi@gmail.com>
2023-05-05 07:29:16 -0700

    shy

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary equivalent
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int king, queen = 0;
+	unsigned long int current;
+/*screw all this*/
+	for (king = 63; king >= 0; king--)
+	{
+		current = n >> king;
+/*c is hard*/
+		if (current & 1)
+		{
+			_putchar('1');
+			queen++;
+		}
+		else if (queen)
+			_putchar('0');
+	}
+	if (!queen)
+		_putchar('0');
+}
+/*this sucks*/
