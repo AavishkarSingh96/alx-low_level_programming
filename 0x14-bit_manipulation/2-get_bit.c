a622e833e53654bb9fa4519216fc9c3d086cf91c
AavishkarSingh <heroaavi@gmail.com>
2023-05-05 07:47:26 -0700

    sour

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - returns index in a decimal number
+ * @n: search
+ * @index: index bit
+ *c is hard
+ * Return: value bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+/*did the milk sour*/
+{
+	int sour;
+/*this is really brave*/
+	if (index > 63)
+		return (-1);
+/*but why*/
+	sour = (n >> index) & 1;
+/*did they agree*/
+	return (sour);
+}
+/*speed is key*/
