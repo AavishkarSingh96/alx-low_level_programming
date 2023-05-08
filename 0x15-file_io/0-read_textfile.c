dfe2290b327a862a2e9fdf3647bd9566240541a6
AavishkarSingh <heroaavi@gmail.com>
2023-05-08 07:33:02 -0700

    torn

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- STDOUT print
+ * @filename: text read
+ * @letters: letter count read
+ * Return: w- actual bytes printed
+ *        0 fail or NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+/*there is a heaven*/	
+{
+	char *tan;
+	ssize_t lo;
+	ssize_t ray;
+	ssize_t rain;
+/*there is no water*/
+	lo = open(filename, O_RDONLY);
+	if (lo == -1)
+		return (0);
+	tan = malloc(sizeof(char) * letters);
+	rain = read(lo, tan, letters);
+	ray = write(STDOUT_FILENO, tan, rain);
+/*nothing to write home*/
+	free(tan);
+	close(lo);
+	return (ray);
+}
+/*comments are really long*/
